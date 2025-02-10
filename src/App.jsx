65a9414b12de585765a20b289934382dc4da5a33
apelic <apelic92@gmail.com>
2025-02-10 14:20:58 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import { useState } from 'react'
+import './App.css'
+import {puppyList} from './data.js'
+
+
+function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppies)
+
+  return (
+    <>
+      <div>
+        {
+          puppies.map((puppy) => {
+            return <p>{puppy.name}</p>
+          })
+        }
+      </div>
+     
+    </>
+  )
+}
+
+export default App

3505375fa29ed08fd3ed8f8bd67c0758483bd4b7
apelic <apelic92@gmail.com>
2025-02-10 15:12:06 -0600

    rendered puppy data

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,31 @@ import {puppyList} from './data.js'
 
 
 function App() {
-  const [puppies, setPuppies] = useState(puppyList)
-  console.log(puppies)
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId);
+
+  console.log(featuredPup)
+  console.log("puppyList: ", puppyList);
 
   return (
       <div className="App"> 
+
         {
           puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>
+            return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
           })
         }
+
+       {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+       )}
       </div>
      
   );
